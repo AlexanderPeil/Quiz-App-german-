72364bb89ca068b5419b4460fa423e17601dfb33
Alexander Peil <you@example.com>
2023-01-06 09:46:37 +0100

    Added question & answers (as JSON Array)

---
+++ script.js
@@ -0,0 +1,151 @@
+let questions = [
+    {
+        "question": "Was ist die Hauptstadt von Spanien",
+        "answer_1": "Barcelona",
+        "answer_2": "Bilbao",
+        "answer_3": "Madrid",
+        "answer_4": "Grenada",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist die Landeshauptstadt von Baden-Württemberg",
+        "answer_1": "Karlsruhe",
+        "answer_2": "Stuttgart",
+        "answer_3": "Mannheim",
+        "answer_4": "Freiburg",
+        "right_answer": 2
+    },
+    {
+        "question": "Was das flächenmäßig größte Land der Erde",
+        "answer_1": "China",
+        "answer_2": "USA",
+        "answer_3": "Indien",
+        "answer_4": "Russland",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist die Landeshauptstadt von Niedersachsen",
+        "answer_1": "Hannover",
+        "answer_2": "Wolfsburg",
+        "answer_3": "Osnabrück",
+        "answer_4": "Braunschweig",
+        "right_answer": 1
+    },
+    {
+        "question": "Was ist die Hauptstadt von Australien",
+        "answer_1": "Sydney",
+        "answer_2": "Melbourne",
+        "answer_3": "Adelaide",
+        "answer_4": "Canberra",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist die Hauptstadt von der Niederlande",
+        "answer_1": "Eindhoven",
+        "answer_2": "Amsterdam",
+        "answer_3": "Rotterdam",
+        "answer_4": "Den Haag",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist die Hauptstadt von Italien",
+        "answer_1": "Rom",
+        "answer_2": "Turin",
+        "answer_3": "Mailand",
+        "answer_4": "Venedig",
+        "right_answer": 1
+    },
+    {
+        "question": "Welche ist keine Hauptstadt in Südamerika",
+        "answer_1": "Brasilia",
+        "answer_2": "Rio de Janeiro",
+        "answer_3": "Buenos Aires",
+        "answer_4": "Lima",
+        "right_answer": 2
+    },
+    {
+        "question": "Was ist die Landeshauptstadt von Thüringen",
+        "answer_1": "Jena",
+        "answer_2": "Weimar",
+        "answer_3": "Gera",
+        "answer_4": "Erfurt",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche ist keine Hauptstadt in Asien",
+        "answer_1": "Jakarta",
+        "answer_2": "Manila",
+        "answer_3": "Ulaanbaatar",
+        "answer_4": "Shanghai",
+        "right_answer": 4
+    },
+    {
+        "question": "Welche ist keine Hauptstadt in Europa",
+        "answer_1": "Oslo",
+        "answer_2": "Malmö",
+        "answer_3": "Prag",
+        "answer_4": "Budapest",
+        "right_answer": 2
+    },
+    {
+        "question": "Welche ist keine Hauptstadt in Europa",
+        "answer_1": "Bukarest",
+        "answer_2": "Chisinau",
+        "answer_3": "Manchester",
+        "answer_4": "Wien",
+        "right_answer": 3
+    },
+    {
+        "question": "Was ist die Landeshauptstadt von Sachsen",
+        "answer_1": "Leipzig",
+        "answer_2": "Chemnitz",
+        "answer_3": "Zwickau",
+        "answer_4": "Dresden",
+        "right_answer": 4
+    },
+    {
+        "question": "Was ist die Landeshauptstadt von Rheinland-Pflaz",
+        "answer_1": "Mainz",
+        "answer_2": "Kaiserslautern",
+        "answer_3": "Trier",
+        "answer_4": "Worms",
+        "right_answer": 1
+    },
+    {
+        "question": "Was ist die Landeshauptstadt von Nordrhein-Westfalen",
+        "answer_1": "Düsseldorf",
+        "answer_2": "Dortmund",
+        "answer_3": "Essen",
+        "answer_4": "Köln",
+        "right_answer": 1
+    },
+    {
+        "question": "Was ist die Landeshauptstadt von Hessen",
+        "answer_1": "Darmstadt",
+        "answer_2": "Frankfurt",
+        "answer_3": "Wiesbaden",
+        "answer_4": "Kassel",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche ist keine Hauptstadt in Afrika",
+        "answer_1": "Rabat",
+        "answer_2": "Accra",
+        "answer_3": "Alexandria",
+        "answer_4": "Tunis",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche ist keine Hauptstadt auf der Arabischen Halbinsel",
+        "answer_1": "Riad",
+        "answer_2": "Doha",
+        "answer_3": "Abu Dhabi",
+        "answer_4": "Dubai",
+        "right_answer": 4
+    },
+];
+
+
+function init() {
+    
+}
